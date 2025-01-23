59447d154d671af08fe97204f325097622f752a1
briannaordaz <briannaordaz5@gmail.com>
2025-01-22 22:57:37 -0800

    Prompted user Input

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace COMP003A.Assignment2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("**************************************************");
+        
+        Console.WriteLine("Welcome to the Discount Calculator!");
+        
+        Console.WriteLine("**************************************************");
+        
+        Console.WriteLine("What is your first name?");
+        string userFirstName = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name?");
+        string userLastName = Console.ReadLine();
+        
+        Console.WriteLine("How old are you?");
+        string userAge = Console.ReadLine();
+        
+        Console.WriteLine("What is the price of the item you're interested in?");
+        string userPrice = Console.ReadLine();
+        
+        Console.WriteLine("Are you a student?");
+        
+        
+        
+    }
+}
